75475f9673c21b30a2c68834589d7b55e190a42b
thisisaaronland <devnull@localhost>
2019-07-05 06:29:37 -0700

    just assume account.Account is a struct for now

--- account/account.go
+++ account/account.go
@@ -3,6 +3,9 @@ package account
 import (
 	"errors"
 	"github.com/aaronland/go-password"
+	"github.com/aaronland/go-ucd-username"
+	"net/mail"
+	"time"
 )
 
 const ACCOUNT_STATUS_PENDING int = 0
@@ -10,20 +13,26 @@ const ACCOUNT_STATUS_ENABLED int = 1
 const ACCOUNT_STATUS_DISABLED int = 2
 const ACCOUNT_STATUS_DELETED int = 3
 
+// this will become an interface when the dust settles
+
 type Account struct {
-	ID             int64           `json:"id"`
-	Name           string          `json:"name"`
-	NameURISafe    string          `json:"name_urisafe"`
-	EmailAddresses []*EmailAddress `json:"email_addresses"`
-	Password       *Password       `json:"password"`
-	Created        int64           `json:"created"`
-	LastModified   int64           `json:"lastmodified"`
-	Status         int             `json:"status"`
+	ID           int64     `json:"id"`
+	Address      *Address  `json:"address"`
+	Password     *Password `json:"password"`
+	Username     *Username `json:"username"`
+	Created      int64     `json:"created"`
+	LastModified int64     `json:"lastmodified"`
+	Status       int       `json:"status"`
 }
 
-type EmailAddress struct {
+type Username struct {
+	Raw  string `json:"raw"`
+	Safe string `json:"safe"`
+}
+
+type Address struct {
+	Model     string `json:"model"`
 	Address   string `json:"address"`
-	Primary   bool   `json:"primary"`
 	Confirmed bool   `json:"confirmed"`
 }
 
@@ -34,6 +43,72 @@ type Password struct {
 	LastModified int64  `json:"lastmodified"`
 }
 
-func (a *Account) GetPassword() (password.Password, error) {
-	return nil, errors.New("Please write me")
+func NewAccount(email_raw string, password_raw string, username_raw string) (*Account, error) {
+
+	emails, err := mail.ParseAddressList(email_raw)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(emails) != 1 {
+		return nil, errors.New("Invalid email address string")
+	}
+
+	ucd, err := username.NewUCDUsername()
+
+	if err != nil {
+		return nil, err
+	}
+
+	ucd.Debug = false
+	ucd.AllowSpaces = false
+	ucd.AllowPunctuation = false
+
+	username_safe, err := ucd.Translate(username_raw)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bcrypt_pswd, err := password.NewBCryptPassword(password_raw)
+
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+
+	uname := &Username{
+		Raw:  username_raw,
+		Safe: username_safe,
+	}
+
+	addr := &Address{
+		Model:     "email",
+		Address:   emails[0].Address,
+		Confirmed: false,
+	}
+
+	pswd := &Password{
+		Model:        "bcrypt",
+		Digest:       bcrypt_pswd.Digest(),
+		Salt:         bcrypt_pswd.Salt(),
+		LastModified: now.Unix(),
+	}
+
+	acct := &Account{
+		Address:      addr,
+		Password:     pswd,
+		Username:     uname,
+		Created:      now.Unix(),
+		LastModified: now.Unix(),
+		Status:       ACCOUNT_STATUS_PENDING,
+	}
+
+	return acct, nil
+}
+
+func (acct *Account) GetPassword() (password.Password, error) {
+	return password.NewBCryptPasswordFromDigest(acct.Password.Digest, acct.Password.Salt)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/aaronland/go-http-rewrite v0.0.1
 	github.com/aaronland/go-http-sanitize v0.0.1
 	github.com/aaronland/go-password v0.0.1
+	github.com/aaronland/go-ucd-username v0.1.0
 	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980
 	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb // indirect
 )
